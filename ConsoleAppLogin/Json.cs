ce62705228362dc71041caa053e06c65d9ecf62d
StephenGregan <grego@live.ie>
2018-05-23 10:01:44 +0100

    initial commit

---
+++ ConsoleAppLogin/Json.cs
@@ -0,0 +1,121 @@
+﻿// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+//
+//    using ConsoleAppLogin;
+//
+//    var welcome = Welcome.FromJson(jsonString);
+
+namespace ConsoleAppLogin
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Microsoft.Azure.Search;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public class Json
+    {
+        public partial class Welcome
+        {
+            [System.ComponentModel.DataAnnotations.Key]
+            [IsFilterable]
+            [JsonProperty("id")]
+            public long Id { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("uuid")]
+            public string Uuid { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("versionValue")]
+            public long VersionValue { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("company.id")]
+            public long CompanyId { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("createdBy")]
+            public string CreatedBy { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("createdDate")]
+            public string CreatedDate { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("lastModifiedBy")]
+            public string LastModifiedBy { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("lastModifiedDate")]
+            public string LastModifiedDate { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("accountingReference")]
+            public object AccountingReference { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("parentAccountingReference")]
+            public object ParentAccountingReference { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("contactName")]
+            public object ContactName { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("website")]
+            public object Website { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("notes")]
+            public object Notes { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("paymentTerms")]
+            public object PaymentTerms { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("paymentDaysDue")]
+            public object PaymentDaysDue { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("purchaseOrderNumber")]
+            public object PurchaseOrderNumber { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("accountingSickLeaveCode")]
+            public object AccountingSickLeaveCode { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("billingMethod.id")]
+            public string BillingMethodId { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("number")]
+            public string Number { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("address")]
+            public string Address { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("email")]
+            public string Email { get; set; }
+
+            //[JsonProperty("numbers")]
+            //public Number[] Numbers { get; set; }
+
+            [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+            [JsonProperty("addresses")]
+            public object[] Addresses { get; set; }
+        }
+    }
+}
+         
+        
+
---
+++ ConsoleAppLogin/Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static ConsoleAppLogin.Json;
+
+namespace ConsoleAppLogin
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            var shows = LoginAndDeserializeJson();
+            Console.WriteLine(shows);
+
+        }
+
+        static string LoginAndDeserializeJson()
+        {
+
+            using (var client = new WebClientEx())
+            {
+                var values = new NameValueCollection
+            {
+                { "j_username", "[PASSWORD]" },
+                { "j_password", "[USERNAME]" },
+            };
+                // Authenticate
+                client.UploadValues("https://tie.interpreterintelligence.com/j_spring_security_check", values);
+                Console.WriteLine("Successfully logged in to ii.....");
+                Console.WriteLine();
+                // Download desired page
+                var json = client.DownloadString("https://tie.interpreterintelligence.com:443/api/visit");
+
+                //Not Working...
+                //var token = JToken.Parse(json);
+
+                //if (token.Type == JTokenType.Object)
+                //    token = new JArray(token);
+
+                //var a = token.ToObject<DataTable>();
+
+                var results = JsonConvert.DeserializeObject<Json>(json);
+                Console.WriteLine("Deserialized Json : " + json);
+
+                return json;
+            }
+        }
+
+        /// <summary>
+        /// A custom WebClient featuring a cookie container
+        /// </summary>
+
+        public class WebClientEx : WebClient
+        {
+            public CookieContainer CookieContainer { get; private set; }
+
+            public WebClientEx()
+            {
+                CookieContainer = new CookieContainer();
+            }
+
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                var request = base.GetWebRequest(address);
+                if (request is HttpWebRequest)
+                {
+                    (request as HttpWebRequest).CookieContainer = CookieContainer;
+                }
+                return request;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
